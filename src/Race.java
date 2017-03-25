838da59eec707b6974db6a163ba35fbb6345e0fb
Robert imbrie <robert.m.imbrie@gmail.com>
2017-03-25 18:37:43 -0500

    Update Race.java

--- src/Race.java
+++ src/Race.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Race{
 	ArrayList<Competitor> competitors;
-	BufferedWriter logWriter = null;
+	transient BufferedWriter logWriter = null;
 	
 	
 	public void trigger(int channel, long time){
