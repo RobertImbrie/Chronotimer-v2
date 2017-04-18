ba016526c7421c992519bc42167a14c2b6fc31a9
Bryce Butenhof <butenhof@uwm.edu>
2017-04-18 14:22:56 -0500

    Created Skeleton methods for TestGrp
    All test methods have been added

--- src/TestGrp.java
+++ src/TestGrp.java
@@ -1,5 +1,62 @@
 package src;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class TestGrp {
-
+	
+	@Test
+	public void testTrigger(){
+		
+	}
+	
+	@Test 
+	public void testCompetitorList(){
+		
+	}
+	
+	@Test 
+	public void testClear(){
+		
+	}
+	
+	@Test
+	public void testRemoveCompetitorByBib(){
+		
+	}
+	
+	@Test
+	public void testRemoveCompetitorByPos(){
+		
+	}
+	
+	@Test
+	public void testDidNotFinish(){
+		
+	}
+	
+	@Test
+	public void testEnd(){
+		
+	}
+	
+	@Test
+	public void testStart(){
+		
+	}
+	
+	@Test
+	public void testReset(){
+		
+	}
+	
+	@Test
+	public void testRuntime(){
+		
+	}
+	
+	@Test
+	public void testToString(){
+		
+	}
 }
