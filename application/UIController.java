cd677cd900f9a16f8cd8f6b77868013faf75b49b
Robert imbrie <robert.m.imbrie@gmail.com>
2017-04-23 22:08:11 -0500

    Update UIController.java

--- application/UIController.java
+++ application/UIController.java
@@ -6,13 +6,17 @@ import src.Chronotimer;
 
 public class UIController {
 	BufferedWriter logWriter;
+	CmdParser commandParser;
 	
 	UIController(BufferedWriter b){
 		logWriter = b;
 		Chronotimer c = new Chronotimer(System.nanoTime(), logWriter);
+		commandParser = new CmdParser(logWriter);
 	}
 	public String command(String c, long l){
 		debug("command called");
+		c = l.toString() + c;
+		String result = commandParser.input( c.split(" ") );
 		return "'" + c + "' was called at " + l;
 	}
 	 public String toggleChannel(int chan, boolean toState, long l){
@@ -36,7 +40,35 @@ public class UIController {
 	 public String swap(long l){
 		 return "";
 	 }
-	 
+	
+	public void runFile(String filePath){
+		boolean fileBeenRead = false;
+		while (!fileBeenRead) { //loops until user enters a valid filepath
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(filePath));
+				for (String nextLine = reader.readLine(); nextLine != null && !nextLine.equalsIgnoreCase("EXIT"); nextLine = reader.readLine())
+					String[] command = nextLine.split("\\s+");
+					long time = parseTime(command[0]);
+					for(i = 1; i<command.length; i++)
+						nextLine += command[i];
+					command( nextLine, time);
+				}
+				reader.close();
+			}
+			catch (FileNotFoundException e)
+				System.out.println("Invalid filepath");
+
+	}
+
+	  /** Turns time formatted in HH:MM:SS.S to a long */
+  	public long parseTime(String time){
+    		String[] timeString = time.split(":|\\.");
+    		return (Long.parseLong(timeString[0]) * 36000L //Convert to a single time unit (tenth-seconds), which is then converted to nanoseconds
+     		 + Long.parseLong(timeString[1]) * 600L
+   		 + Long.parseLong(timeString[2]) * 10L
+  		 + Long.parseLong(timeString[3]))
+ 		 * 100000000L;
+ 	}
 	 
 	 //----------
 	 private void debug(String s){
