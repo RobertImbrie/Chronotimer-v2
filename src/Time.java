a140ee5726e411abcb4af2eea10d63ac1c6fb43c
Nicholas Michalowski <michal67@uwm.edu>
2017-05-03 17:53:23 -0500

    Update Time.java

--- src/Time.java
+++ src/Time.java
@@ -25,8 +25,11 @@ public class Time {
 	//Parses a  long val to time.
 	public static String parseTime(long a) { // Parse any integer
 		String result;
-		final double seconds = ((double) a / 1000000000);
-		result = new DecimalFormat("#.###").format(seconds) + " Seconds";
+		final int hours = (int) a/3600000000000;
+		final int minutes = (int) (a%3600000000000)/60000000000;
+		final int seconds = (int) ((a%3600000000000)%60000000000)/1000000000;
+		final int milliseconds = (int) (((a%3600000000000)%60000000000)%1000000000)/1000000;
+		result = hours+":"+minutes+":"+seconds+"."+milliseconds;
 		return result;
 	}
 	
