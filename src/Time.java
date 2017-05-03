ba3111a96b8b20e6117e467305a77414a99ae2f1
Nicholas Michalowski <michal67@uwm.edu>
2017-05-03 17:33:36 -0500

    Update Time.java

--- src/Time.java
+++ src/Time.java
@@ -4,14 +4,17 @@ import java.text.DecimalFormat;
 public class Time {
 	private long startTime; // Stores TimeStarted
 
+	//Constructs a new time class and records the start time in nanotime
 	public Time() {
 		startTime = System.nanoTime();
 	}
 
+	//Updates the startTime value to be the current nanoTime
 	public void ToggleTimer() {
 		startTime = System.nanoTime();
 	}
 
+	//Parses the currentTime into seconds
 	public String parseTime() { // Parse current time
 		String result;
 		final double seconds = ((double) this.getTime() / 1000000000);
@@ -19,13 +22,15 @@ public class Time {
 		return result;
 	}
 
+	//Parses a  long val to time.
 	public static String parseTime(long a) { // Parse any integer
 		String result;
 		final double seconds = ((double) a / 1000000000);
 		result = new DecimalFormat("#.###").format(seconds) + " Seconds";
 		return result;
 	}
-
+	
+	//Returns the current time as a long.
 	public long getTime() {
 		return System.nanoTime() - startTime;
 	}
